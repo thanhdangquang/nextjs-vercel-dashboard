556d84f3b039a81defd3b79a6017a0fb1b31bb74
thanhdangquang <kwanthanh2101@gmail.com>
2025-01-16 15:48:57 +0700

    login and out

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,8 @@ import Link from "next/link";
 import { lusitana } from "@/app/ui/fonts";
 import styles from "@/app/ui/home.module.css";
 import Image from "next/image";
+import { Button } from "@/components/ui/button";
+
 export default function Page() {
   return (
     <main className="flex min-h-screen flex-col p-6">
@@ -13,7 +15,7 @@ export default function Page() {
       <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
         <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
           <div className={styles.shape} />
-          <p
+          {/* <p
             className={`${lusitana.className} text-xl text-gray-800 md:text-3xl md:leading-normal`}
           >
             <strong>Welcome to Acme.</strong> This is the example for the{" "}
@@ -21,12 +23,16 @@ export default function Page() {
               Next.js Learn Course
             </a>
             , brought to you by Vercel.
-          </p>
+          </p> */}
+          <Button asChild>
+            <Link href="/dashboard">Dashboard</Link>
+          </Button>
+
           <Link
             href="/login"
             className="flex items-center gap-5 self-start rounded-lg bg-blue-500 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-blue-400 md:text-base"
           >
-            <span>Log in</span> <ArrowRightIcon className="w-5 md:w-6" />
+            <span>Log in</span>
           </Link>
         </div>
         <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
--- app/ui/dashboard/nav-links.tsx
+++ app/ui/dashboard/nav-links.tsx
@@ -7,6 +7,7 @@ import {
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import clsx from "clsx";
+
 // Map of links to display in the side navigation.
 // Depending on the size of the application, this would be stored in a database.
 const links = [
